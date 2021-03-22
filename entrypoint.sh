fdc199f9d868ff472e1887996d9faf5c7c4dca05
Dustin Rogers <dustinrogers@MacBook-Pro.local>
2021-03-22 16:32:51 -0700

    not filling params string

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,31 +11,31 @@ fi
 # ARG - Trivy Suppressions List File Path
 if [ ! -z "$2" ]
 then
-	PARAMS_STRING+="--suppressions-path=$2"
+	PARAMS_STRING+=" --suppressions-path=$2"
 fi
 
 # ARG - Create Github Issues Bool
 if [ ! -z "$3" ]
 then
-	PARAMS_STRING+="--github=$3"
+	PARAMS_STRING+=" --github=$3"
 fi
 
 # ARG - Minimum Severity to create GH issues
 if [ ! -z "$4" ]
 then
-	PARAMS_STRING+="--minSeverityGithub=$4"
+	PARAMS_STRING+=" --minSeverityGithub=$4"
 fi
 
 # ARG - Create Slack Notifications Bool
 if [ ! -z "$5" ]
 then
-	PARAMS_STRING+="--slack=$5"
+	PARAMS_STRING+=" --slack=$5"
 fi
 
 # ARG - Minimum Severity to create Slack alerts
 if [ ! -z "$6" ]
 then
-	PARAMS_STRING+="--minSeveritySlack=$6"
+	PARAMS_STRING+=" --minSeveritySlack=$6"
 fi
 
 echo "$PARAMS_STRING"
